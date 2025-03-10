d44af7914f1bcc5c3da94a5f6ecaee5888b34073
moyoyoyom <128235845+moyoyoyom@users.noreply.github.com>
2025-03-10 23:43:27 +0000

    Added a navigation bar

--- HealifyApp/screens/HomeScreen.js
+++ HealifyApp/screens/HomeScreen.js
@@ -1,23 +1,39 @@
-import { StyleSheet, Text, TouchableOpacity, View, navigate } from 'react-native'
-import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  navigate,
+} from "react-native";
+import React from "react";
 
-const HomeScreen = ({navigation}) => {
+const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <Text>Home</Text>
-      <TouchableOpacity onPress={() => navigation.navigate('UserDetails')} style={{borderWidth: 1, padding: 5, width: 100, justifyContent: 'center', alignItems: 'center', marginTop: 100}}>
+      <TouchableOpacity
+        onPress={() => navigation.navigate("UserDetails")}
+        style={{
+          borderWidth: 1,
+          padding: 5,
+          width: 100,
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: 100,
+        }}
+      >
         <Text>Button</Text>
       </TouchableOpacity>
     </View>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
 
 const styles = StyleSheet.create({
   container: {
     padding: 15,
-    justifyContent: 'center',
-    marginVertical: '25%'
-  }
-})
+    justifyContent: "center",
+    marginVertical: "25%",
+  },
+});
