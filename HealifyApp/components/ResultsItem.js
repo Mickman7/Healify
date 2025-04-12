56e3480cd4caceeae27527565576f73e6c1c4da7
moyoyoyom <128235845+moyoyoyom@users.noreply.github.com>
2025-04-12 16:44:18 +0100

    Formatted history items

--- HealifyApp/components/ResultsItem.js
+++ HealifyApp/components/ResultsItem.js
@@ -1,5 +1,6 @@
 import { StyleSheet, Text, View } from "react-native";
 import { format } from "date-fns";
+import { get } from "react-native/Libraries/TurboModule/TurboModuleRegistry";
 
 const ResultsItem = ({ result }) => {
   //Initialisations
@@ -8,7 +9,6 @@ const ResultsItem = ({ result }) => {
 
   //Handlers
   const getStage = (eGFR) => {
-    console.log(eGFR);
     const stage =
       eGFR > 90
         ? "1"
@@ -21,21 +21,48 @@ const ResultsItem = ({ result }) => {
         : eGFR >= 15 && eGFR <= 29
         ? "4"
         : "5";
-    console.log(stage);
     return stage;
+  };
+
+  const getStageColour = (stage) => {
+    if (stage == "1") return styles.greenContainerStyle;
+    if (stage == "2") return styles.yellowContainerStyle;
+    if (stage == "3a" || stage == "3b")
+      return styles.darkerYellowContainerStyle;
+    if (stage == "4") return styles.organgeContainerStyle;
+    if (stage == "5") return styles.redContainerStyle;
   };
 
   //View
   return (
     <View style={styles.resultBackground}>
       <Text style={styles.formattedTextStyle}>{formattedDate}</Text>
-      <Text style={styles.stageIndicatorStyle}>
+      <Text
+        style={[
+          styles.stageIndicatorStyle,
+          getStageColour(getStage(result.eGFRLevel)),
+        ]}
+      >
         Stage {getStage(result.eGFRLevel)}
       </Text>
       <Text style={styles.formattedTextStyle}>Your eGFR:</Text>
-      <Text>{result.eGFRLevel}</Text>
+      <View style={styles.sameLineStyle}>
+        <Text style={styles.numericStyle}>{result.eGFRLevel}</Text>
+        <Text style={[styles.formattedTextStyle, styles.unitStyle]}>
+          {" "}
+          mL/min/1.73m²
+        </Text>
+      </View>
       <Text style={styles.formattedTextStyle}>Your Creatinine Levels:</Text>
-      <Text>{Math.ceil(result.creatineLevel)}</Text>
+      <View style={styles.sameLineStyle}>
+        <Text style={styles.numericStyle}>
+          {Math.ceil(result.creatineLevel)}
+        </Text>
+        <Text style={[styles.formattedTextStyle, styles.unitStyle]}>
+          {" "}
+          µmol/L
+        </Text>
+      </View>
     </View>
   );
 };
@@ -52,14 +79,45 @@ const styles = StyleSheet.create({
     height: "75%",
     marginTop: 25,
     width: 150,
+    backgroundColor: "white",
   },
   formattedTextStyle: {
     marginTop: 10,
     marginLeft: 10,
+    fontSize: 12,
   },
   stageIndicatorStyle: {
     alignSelf: "center",
-    marginTop: 5,
+    marginTop: 10,
     marginBottom: 5,
+    padding: 7,
+    borderRadius: 5,
+  },
+  greenContainerStyle: {
+    backgroundColor: "#4C9A29",
+  },
+  yellowContainerStyle: {
+    backgroundColor: "#FCC333",
+  },
+  darkerYellowContainerStyle: {
+    backgroundColor: "#FDBD20",
+  },
+  organgeContainerStyle: {
+    backgroundColor: "#E57C02",
+  },
+  redContainerStyle: {
+    backgroundColor: "#D94214",
+  },
+  numericStyle: {
+    fontSize: 18,
+    marginLeft: 12,
+  },
+  sameLineStyle: {
+    flexDirection: "row",
+  },
+  unitStyle: {
+    marginTop: 5,
+    marginBottom: 0,
+    marginLeft: 0,
   },
 });
