b4dd4802f89006d0aa96fc506291098492b665f9
moyoyoyom <128235845+moyoyoyom@users.noreply.github.com>
2025-04-14 20:03:43 +0100

    Made a placeholder text for when users have no past calculations

--- HealifyApp/screens/AuthenticationScreen.js
+++ HealifyApp/screens/AuthenticationScreen.js
@@ -1,155 +1,188 @@
-import { StyleSheet, Text, View, Alert, ImageBackground, TouchableOpacity } from 'react-native'
-import React, { useState, useEffect, useContext } from 'react'
-import { UserTypeContext } from '../App'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Alert,
+  ImageBackground,
+  TouchableOpacity,
+} from "react-native";
+import React, { useState, useEffect, useContext } from "react";
+import { UserTypeContext } from "../App";
 
-import Form from '../components/Form'
-import {createUserWithEmailAndPassword, signInWithEmailAndPassword, sendPasswordResetEmail,onAuthStateChanged, signOut } from "firebase/auth";
-import { collection,getDoc, addDoc, doc, serverTimestamp, firestore, setDoc } from 'firebase/firestore';
+import Form from "../components/Form";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  sendPasswordResetEmail,
+  onAuthStateChanged,
+} from "firebase/auth";
+import { getDoc, doc, setDoc } from "firebase/firestore";
 
-import { FIREBASE_APP, FIREBASE_AUTH, FIREBASE_DB } from '../FirebaseConfig'; 
+import { FIREBASE_AUTH, FIREBASE_DB } from "../FirebaseConfig";
 
+const AuthenticationScreen = ({ navigation }) => {
+  const [user, setUser] = useState(null);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLogin, setIsLogin] = useState(false);
 
-const AuthenticationScreen = ({navigation, route}) => {
+  const auth = FIREBASE_AUTH;
+  const { userType } = useContext(UserTypeContext);
 
-    const [user, setUser] = useState(null); 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [isLogin, setIsLogin] = useState(false);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(FIREBASE_AUTH, (user) => {
+      setUser(user);
+    });
 
-    // const { userType } = route.params;
+    return unsubscribe;
+  }, []);
 
-
-    const auth = FIREBASE_AUTH;
-    const { userType } = useContext(UserTypeContext);
-
-
-  
-    useEffect(() => {
-        const unsubscribe = onAuthStateChanged(FIREBASE_AUTH, (user) => {
-          setUser(user);
-        });
-      
-        return unsubscribe;
-      }, []);
-
-    const handleAuthentication = async () => {
-      if (!email || !password) {
-          Alert.alert("Email or password is empty.");
-          return;
-        }
-      if (!userType) {
-        Alert.alert('Error', 'Please select a user type.');
-        return;
-      }
-      
-      
-      
-        try {
-          if (isLogin) {
-            const userCredential = await signInWithEmailAndPassword(FIREBASE_AUTH, email, password);
-            const user = userCredential.user;
-            const userDocRef = doc(FIREBASE_DB, 'users', user.uid);
-            const userDoc = await getDoc(userDocRef);
-            if(userDoc.exists()){
-              const userData = userDoc.data();
-              const storedUserType = userData.userType;
-              if(userType === storedUserType){
-                console.log('User signed in successfully!');
-                navigation.navigate('BottomTabs')
-              }else{
-                Alert.alert('Error', 'The selected user type does not match your account.');
-              }
-            }else{
-              Alert.alert('Error', 'User data not found.');
-            }
-            
-          } else {
-            const userCredential = await createUserWithEmailAndPassword(FIREBASE_AUTH, email, password);
-            const user = userCredential.user;
-            const uid = user.uid; //Making sure the collection uid is the same as the auth uid
-
-            const userDocRef = doc(FIREBASE_DB, 'users', uid);
-            await setDoc(userDocRef, {
-              userId: user.uid, 
-              email: user.email,
-              userType: userType,
-            });
-            console.log('User details submitted successfully!');
-            navigation.navigate('Home');
-           
-            console.log('User created successfully!');
-          }
-          
-        } catch (error) {
-          console.error('Authentication error:', error.message);
-        }
-    };
-
-
-    const handleForgotPassword = async() => {
-      try{
-        await sendPasswordResetEmail(FIREBASE_AUTH, email);
-        Alert.alert('Password reset email sent')
-
-      }catch(err) {
-        console.error(err);
-      }
-      
+  const handleAuthentication = async () => {
+    if (!email || !password) {
+      Alert.alert("Email or password is empty.");
+      return;
     }
+    if (!userType) {
+      Alert.alert("Error", "Please select a user type.");
+      return;
+    }
+
+    try {
+      if (isLogin) {
+        const userCredential = await signInWithEmailAndPassword(
+          FIREBASE_AUTH,
+          email,
+          password
+        );
+        const user = userCredential.user;
+        const userDocRef = doc(FIREBASE_DB, "users", user.uid);
+        const userDoc = await getDoc(userDocRef);
+        if (userDoc.exists()) {
+          const userData = userDoc.data();
+          const storedUserType = userData.userType;
+          if (userType === storedUserType) {
+            console.log("User signed in successfully!");
+            navigation.navigate("BottomTabs");
+          } else {
+            Alert.alert(
+              "Error",
+              "The selected user type does not match your account."
+            );
+          }
+        } else {
+          Alert.alert("Error", "User data not found.");
+        }
+      } else {
+        const userCredential = await createUserWithEmailAndPassword(
+          FIREBASE_AUTH,
+          email,
+          password
+        );
+        const user = userCredential.user;
+        const uid = user.uid; //Making sure the collection uid is the same as the auth uid
+
+        const userDocRef = doc(FIREBASE_DB, "users", uid);
+        await setDoc(userDocRef, {
+          userId: user.uid,
+          email: user.email,
+          userType: userType,
+        });
+        console.log("User details submitted successfully!");
+        navigation.navigate("BottomTabs");
+
+        console.log("User created successfully!");
+      }
+    } catch (error) {
+      console.error("Authentication error:", error.message);
+    }
+  };
+
+  const handleForgotPassword = async () => {
+    try {
+      await sendPasswordResetEmail(FIREBASE_AUTH, email);
+      Alert.alert("Password reset email sent");
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   return (
     <ImageBackground
-      source={require("../assets/LoginBackgroundImg.png")} 
+      source={require("../assets/LoginBackgroundImg.png")}
       style={styles.backgoundImg}
     >
-
       <View style={styles.formContainer}>
-
         <Form
           isLogin={isLogin}
           setIsLogin={setIsLogin}
-          style={{alignItems: 'center', marginTop: 40}}
+          style={{ alignItems: "center", marginTop: 40 }}
         >
+          <Form.InputText
+            label="Email Address"
+            value={email}
+            onChange={setEmail}
+            isPassword={false}
+            style={{
+              width: 350,
+              height: 50,
+              borderColor: "grey",
+              marginTop: 5,
+              marginBottom: 15,
+              backgroundColor: "white",
+            }}
+            textStyle={{ color: "white" }}
+          />
+          <Form.InputText
+            label="Password"
+            value={password}
+            onChange={setPassword}
+            isPassword={true}
+            style={{
+              width: 350,
+              height: 50,
+              borderColor: "grey",
+              marginTop: 5,
+              backgroundColor: "white",
+            }}
+            textStyle={{ color: "white" }}
+          />
 
-        <Form.InputText
-          label='Email Address'
-          value={email}
-          onChange={setEmail}
-          isPassword={false}
-          style={{width: 350, height: 50, borderColor: 'grey', marginTop: 5, marginBottom: 15, backgroundColor: 'white'}}
-          textStyle={{color: 'white'}}
-        />
-        <Form.InputText
-          label='Password'
-          value={password}
-          onChange={setPassword}
-          isPassword={true}
-          style={{width: 350, height: 50, borderColor: 'grey', marginTop: 5, backgroundColor: 'white'}}
-          textStyle={{color: 'white'}}
-        />
-        
-        {isLogin && (
-          <TouchableOpacity onPress={handleForgotPassword}>
-            <Text style={{color: 'white', fontWeight: 'bold', marginLeft: 200, marginVertical: 10}}>Forgot Password?</Text>
-          </TouchableOpacity>
-        )}
+          {isLogin && (
+            <TouchableOpacity onPress={handleForgotPassword}>
+              <Text
+                style={{
+                  color: "white",
+                  fontWeight: "bold",
+                  marginLeft: 200,
+                  marginVertical: 10,
+                }}
+              >
+                Forgot Password?
+              </Text>
+            </TouchableOpacity>
+          )}
 
-
-        <Form.SubmitButton
-          label={isLogin ? 'Login' : 'Create'}
-          onPress={handleAuthentication}
-          style={{backgroundColor: 'white', margin: 15, width: 120, height: 50, padding: 5, textAlign: 'center', borderRadius: 15}}
-          textStyle={{color: 'black', fontWeight: 'bold'}}
-        />
+          <Form.SubmitButton
+            label={isLogin ? "Login" : "Create"}
+            onPress={handleAuthentication}
+            style={{
+              backgroundColor: "white",
+              margin: 15,
+              width: 120,
+              height: 50,
+              padding: 5,
+              textAlign: "center",
+              borderRadius: 15,
+            }}
+            textStyle={{ color: "black", fontWeight: "bold" }}
+          />
         </Form>
       </View>
     </ImageBackground>
+  );
+};
 
-    
-  )
-}
-
-export default AuthenticationScreen
+export default AuthenticationScreen;
 
 const styles = StyleSheet.create({
   formContainer: {
@@ -157,6 +190,6 @@ const styles = StyleSheet.create({
     marginTop: 40,
   },
   backgoundImg: {
-    height: '100%'
-  }
-})
+    height: "100%",
+  },
+});
--- HealifyApp/screens/UserTypeScreen.js
+++ HealifyApp/screens/UserTypeScreen.js
@@ -1,75 +1,72 @@
-import { Pressable, StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native'
-import React, {useState, useEffect, useContext } from 'react'
+import { StyleSheet, Text, TouchableOpacity, View, Image } from "react-native";
+import React, { useContext } from "react";
 
-import { UserTypeContext } from '../App'
+import { UserTypeContext } from "../App";
 
-
-const UserTypeScreen = ({navigation}) => {
-
-    const { setUserType } = useContext(UserTypeContext);
-
-
-  
+const UserTypeScreen = ({ navigation }) => {
+  const { setUserType } = useContext(UserTypeContext);
 
   return (
     <View style={styles.container}>
-        <Image source={require('../assets/AppLogo.png')} style={{marginBottom: 50}}/>
-        <Text style={styles.title}>What type of user are you?</Text>
-        <View style={styles.buttonContainer}>
-            <TouchableOpacity 
-                onPress={() => {
-                    setUserType('Patient')
-                    navigation.navigate('AuthenticationScreen')
-                }}
-                style={styles.button}
-            >
-                <Text style={styles.buttonText}>Patient</Text>
-            </TouchableOpacity>
+      <Image
+        source={require("../assets/AppLogo.png")}
+        style={{ marginBottom: 50 }}
+      />
+      <Text style={styles.title}>What type of user are you?</Text>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          onPress={() => {
+            setUserType("Patient");
+            navigation.navigate("AuthenticationScreen");
+          }}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Patient</Text>
+        </TouchableOpacity>
 
-            <TouchableOpacity 
-                onPress={() => {
-                    setUserType('Clinician')
-                    navigation.navigate('AuthenticationScreen')
-                }}
-                style={styles.button}
-            >
-                <Text style={styles.buttonText}>Clinician</Text>
-            </TouchableOpacity>
-        </View>
-       
+        <TouchableOpacity
+          onPress={() => {
+            setUserType("Clinician");
+            navigation.navigate("AuthenticationScreen");
+          }}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Clinician</Text>
+        </TouchableOpacity>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default UserTypeScreen
+export default UserTypeScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        marginVertical: '25%',
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    title: {
-        fontSize: 25,
-        fontWeight: 'bold'
-    },
-    buttonContainer: {
-        margin: 50,
-        justifyContent: 'space-evenly'
-    },
-    button: {
-        borderWidth: 1,
-        borderRadius: 20,
-        width: 150,
-        height: 75,
-        padding: 10,
-        margin: 15,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#001C45'
-    },
-    buttonText: {
-        color: 'white',
-        fontWeight: 'bold'
-    }
-})
+  container: {
+    marginVertical: "25%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 25,
+    fontWeight: "bold",
+  },
+  buttonContainer: {
+    margin: 50,
+    justifyContent: "space-evenly",
+  },
+  button: {
+    borderWidth: 1,
+    borderRadius: 20,
+    width: 150,
+    height: 75,
+    padding: 10,
+    margin: 15,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#001C45",
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "bold",
+  },
+});
